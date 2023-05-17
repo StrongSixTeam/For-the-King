bd0dbc4368882f4e84b37f710628a5abb5ebe959
허진 <jinnus.heo@gmail.com>
2023-05-17 16:03:41 +0900

    [feat] 땅과 바다 생성
    1. 땅과 바다 구별 가능
    2. 수호의숲, 황금평원, 바다 Material 추가

--- Assets/3.Script/Develop/HJ/HexMember.cs
+++ Assets/3.Script/Develop/HJ/HexMember.cs
@@ -2,16 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum HexDirection
+{
+	/*
+	NW:ÁÂ»ó NE:¿ì»ó
+	W:ÁÂ E: ¿ì
+	SW:ÁÂÇÏ SE:¿ìÇÏ
+	*/
+	NW, NE, W, E, SW, SE
+}
 
 public class HexMember : MonoBehaviour
 {
-	//·»´õ¸µµµ ¿©±â¼­ 
+	
+	public int xNum;
+	public int zNum;
+	public int index;
 
-	Mesh hexMesh;
+	public bool isGround = false;
+	
+	public float elevation;
 
-	void Awake()
-	{
-		hexMesh = GetComponent<MeshFilter>().mesh;
+    public void Setelevation()
+    {
+        if (isGround)
+        {
+			elevation = 0f;
+			Vector3 position = transform.localPosition;
+			position.y = elevation;
+			transform.localPosition = position;
+		}
+
 	}
 
+	[SerializeField] HexMember[] neighbors = new HexMember[6];
+
+	public HexMember GetNeighbor(HexDirection direction)
+	{
+		return neighbors[(int)direction];
+	}
+
+	public void SetNeighbor(HexDirection direction, HexMember neighbor)
+	{
+		neighbors[(int)direction] = neighbor;
+		neighbor.neighbors[(int)direction + 1] = this;
+	}
 }
