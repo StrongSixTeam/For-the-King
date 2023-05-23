cb377c60e5e427a9e47b85f7e56bf781696d215d
허진 <jinnus.heo@gmail.com>
2023-05-23 19:38:50 +0900

    [feat] Astar 이동경로로 플레이어 이동
    1. Astar로 구한 이동경로와 플레이어 이동 메서드 연결
    2. 삭제된 파일 복구

---
+++ Assets/3.Script/Develop/HJ/PlayerController_Jin.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_Jin : MonoBehaviour
+{
+
+    public int myHexNum = 1350; //플레이어가 밟고있는 땅
+    public List<HexMember> targetNodes = new List<HexMember>();
+
+
+    //에이스타 스크립트에서 호출
+    public void StartMove(List<HexMember> finalNodeList)
+    {
+        targetNodes = finalNodeList;
+        StartCoroutine(MoveTargetNode());
+
+    }
+
+    private IEnumerator MoveTargetNode()
+    {
+        for (int i = 0; i < targetNodes.Count;)
+        {
+
+            Rotation(i);
+            while (Vector3.Distance(transform.position, targetNodes[i].transform.position) > 0.01f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetNodes[i].transform.position, 10f * Time.deltaTime);
+                yield return null;
+            }
+            transform.position = targetNodes[i].transform.position;
+            i++;
+        }
+        myHexNum = targetNodes[targetNodes.Count-1].index;
+        targetNodes.Clear();
+        yield break;
+
+    }
+
+
+    private void Rotation(int index)
+    {
+        Vector3 direction = targetNodes[index].transform.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = targetRotation;
+    }
+
+
+}
