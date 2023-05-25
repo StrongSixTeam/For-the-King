6da3cda41f2c3e5d4645571fa7e9a6983216181c
dani <dani2344@sookmyung.ac.kr>
2023-05-25 17:50:08 +0900

    [feat] 플레이어별 UI 만들기
    1. 로딩씬에서 입력한 이름대로 플레이어의 UI 띄우기 가능
    2. 스탯 및 능력치도 slider와 text 연결

---
+++ Assets/3.Script/Develop/KDI/PlayerUI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField] private int order;
+
+    [SerializeField] private PlayerStat playerStat;
+
+    [SerializeField] private Text name;
+    [SerializeField] private Text strength;
+    [SerializeField] private Text intelligence;
+    [SerializeField] private Text awareness;
+    [SerializeField] private Text speed;
+
+    [SerializeField] private Slider hpSlider;
+    [SerializeField] private Slider expSlider;
+    [SerializeField] private Text hpText;
+    [SerializeField] private Text expText;
+
+    private void Update()
+    {
+        if (GameManager.instance.isSettingDone)
+        {
+            playerStat = GameManager.instance.Players[order].GetComponent<PlayerStat>();
+            name.text = playerStat.name;
+            strength.text = playerStat.strength.ToString();
+            intelligence.text = playerStat.intelligence.ToString();
+            awareness.text = playerStat.awareness.ToString();
+            speed.text = playerStat.speed.ToString();
+
+            hpSlider.maxValue = playerStat.maxHp;
+            hpSlider.value = playerStat.nowHp;
+
+            expSlider.maxValue = playerStat.maxExp;
+            expSlider.value = playerStat.nowExp;
+
+            hpText.text = playerStat.nowHp + " / " + playerStat.maxHp;
+            expText.text = playerStat.nowExp + " / " + playerStat.maxExp;
+        }
+        
+    }
+}
