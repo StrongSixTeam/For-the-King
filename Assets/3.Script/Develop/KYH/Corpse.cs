a4ad018f7dadd4bc1cb14d09fecde1c7fbaf2ab4
y00nhye <yoonhye0711@naver.com>
2023-06-06 21:22:44 +0900

    [fix] 퀘스트 라인 수정
    1. 퀘스트 라인 수정 및 엔딩 씬 연결

--- Assets/3.Script/Develop/KYH/Corpse.cs
+++ Assets/3.Script/Develop/KYH/Corpse.cs
@@ -7,6 +7,8 @@ public class Corpse : MonoBehaviour
 {
     [SerializeField] private CaveBattleBox CaveEnemy02;
 
+    private bool isEnd = false;
+
     private void Awake()
     {
         CaveEnemy02 = GameObject.Find("CaveEnemy02").GetComponent<CaveBattleBox>();
@@ -16,13 +18,16 @@ public class Corpse : MonoBehaviour
         if (CaveEnemy02.enemys01.Count == 0 && CaveEnemy02.enemys02.Count == 0 && CaveEnemy02.enemys03.Count == 0)
         {
             Destroy(gameObject);
+            isEnd = true;
         }
     }
 
     private void OnDisable()
     {
-        if (FindObjectOfType<QuestManager>() != null && FindObjectOfType<QuestManager>().questTurn > 2)
+        if (FindObjectOfType<QuestManager>() != null && FindObjectOfType<QuestManager>().questTurn > 2 && isEnd)
         {
+            Debug.Log("갑자기 끝난다고?");
+            
             SceneManager.LoadScene("EndingScene");
         }
     }
