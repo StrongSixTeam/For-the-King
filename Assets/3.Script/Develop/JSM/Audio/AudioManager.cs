2f3c4fd68fd16010e54709725bc7310a8fb3579a
So-ooming <tnals6254@naver.com>
2023-06-07 15:19:32 +0900

    [fix] 아이템 상세 UI 수정

---
+++ Assets/3.Script/Develop/JSM/Audio/AudioManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+}
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] Sound[] BGM = null;
+}
