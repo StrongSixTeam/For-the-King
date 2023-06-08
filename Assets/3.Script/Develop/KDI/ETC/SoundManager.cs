a7dd0e16b4cedf7f32e7b522289347c481264a60
kdi2344 <dani2344@sookmyung.ac.kr>
2023-06-08 11:23:15 +0900

    [feat] 턴넘김 버튼 소리 + 구름 소리
    1. 턴 넘김 버튼 소리 넣음
    2. 구름 소리 넣음
    3. 턴 넘김 됨

--- Assets/3.Script/Develop/KDI/ETC/SoundManager.cs
+++ Assets/3.Script/Develop/KDI/ETC/SoundManager.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager instance = null;
     AudioSource audioSource;
     public AudioClip audioButton;
+    public AudioClip audioCloud;
+    public AudioClip audioTurn;
     private void Awake()
     {
+        instance = this;
         TryGetComponent(out audioSource);
     }
 
     public void PlayBtn()
     {
         audioSource.clip = audioButton;
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+
+    public void PlayCloud()
+    {
+        audioSource.clip = audioCloud;
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+
+    public void PlayTurn()
+    {
+        audioSource.clip = audioTurn;
         audioSource.PlayOneShot(audioSource.clip);
     }
 }
